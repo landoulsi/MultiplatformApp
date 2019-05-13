8ebb9f2f7342e7812a4a881c24be06db224ca3d6
Ahmed Landoulsi <ahmed.landoulsi@careem.com>
2019-05-13 12:46:30 +0400

    Add coroutines

--- SharedCode/src/commonMain/kotlin/common.kt
+++ SharedCode/src/commonMain/kotlin/common.kt
@@ -1,7 +1,12 @@
 package org.kotlin.mpp.mobile
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
 expect fun platformName(): String
 
 fun createApplicationScreenMessage() : String {
+    GlobalScope.launch {
+    }
     return "Kotlin Rocks on ${platformName()}"
 }
